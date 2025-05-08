a3beb4967d16212e59d29db7d2fa1c6e11999b94
Shreyan Parajuli <shreyanparajuli@sysnefo.com>
2025-05-08 13:47:40 +0545

    add basic search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-import OpCodes from "./data/opcodes"
+import opcodes from "./data/opcodes"
 import {useState} from "react"
 
 
-const [searchParameter, setSearchParameter] = useState("")
 
-const search = (data) => {
-  setSearchParameter(prev => data.target.value);
-}
 
 
 function App() {
+
+  const [searchParameter, setSearchParameter] = useState("")
+
+const search = (data) => {
+  setSearchParameter(prev => data.target.value);
+  console.log(searchParameter)
+}
+const opcode = opcodes.map(data =>{
+  if(searchParameter == ""){
+    return <>{data.opcode} : {data.mnemonic} <br /></>
+  } else {
+    if(searchParameter == data.opcode || searchParameter == data.mnemonic){
+      return <>{data.opcode} : {data.mnemonic} <br /></>
+    }
+  } 
+  
+})
+
   return (
     <>  
       <input type="text" onChange={search} /> <br />
-      {a}
+
+      {opcode}
     </>
   )
 }
